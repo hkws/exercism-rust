f931429990d6e5dc18dff972b98266b367ff1f17
hkws <hkws0130@gmail.com>
2020-05-18 23:21:43 +0900

    add memo

--- etl/src/lib.rs
+++ etl/src/lib.rs
@@ -1,5 +1,6 @@
 use std::collections::BTreeMap;
 
+
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
     let mut map = BTreeMap::new();
     for (key, value) in h.iter() {
@@ -10,3 +11,16 @@ pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
     }
     map
 }
+
+// mapは.iter()でpythonでいう.items()ができる
+// いいなと思った解は以下 https://exercism.io/tracks/rust/exercises/etl/solutions/b4ac6bcd0ac649a9b398b4ef5a4f8ca3
+// pub fn transform(old: &BTreeMap<i32, Vec<String>>) -> BTreeMap<String, i32> {
+//     old.iter()
+//        .flat_map(|(score, letters)| {
+//            letters.iter().map(move |letter| {
+//                (letter.to_lowercase(), *score)
+//            })
+//        })
+//        .collect()
+// }
+// flat_mapは各itemにFnMutを適用した上で、その返り値がiteratorの場合にそれをflatにする

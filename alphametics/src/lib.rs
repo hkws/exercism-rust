034208b296e3ba710d612f7eca6e6eaaa853a69d
hkws <hkws0130@gmail.com>
2020-07-23 00:27:21 +0900

    alphametics wip

--- alphametics/src/lib.rs
+++ alphametics/src/lib.rs
@@ -1,4 +1,37 @@
+// むずすぎるので保留
+// そのうち再チャレンジする
+
+use std::slice::Permutations;
 use std::collections::HashMap;
+use itertools::Itertools;
+
+struct MapFactory {
+
+}
+
+
+
+struct Solver {
+    characters: Vec<char>,
+    lefts: Vec<String>,
+    right: String
+}
+
+impl Solver {
+    pub fn new(lefts: Vec<String>, right: String) -> Self {
+        let sets = Vec::new();
+        for token in lefts.iter().chain([right].into_iter()) {
+            for ch in token.chars() {
+                sets.push(ch);
+            }
+        }
+        Self {
+            characters: sets,
+            lefts, 
+            right
+        }
+    }
+}
 
 pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
     let input_string: String = input.chars().filter(|c| c.is_whitespace()).collect();
@@ -6,7 +39,7 @@ pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
     if leftright.len() != 2 {
         return None
     }
-    let lefts = leftright[0].split("+").collect();
-    let rights = leftright[1];
-
+    let lefts = leftright[0].split("+").map(|token| token.to_string()).collect();
+    let right = leftright[1].to_string();
+    let solver = Solver::new(lefts, right);
 }
